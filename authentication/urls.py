4a57970301d07e8d4e3aa4a05cde05dea492241a
Priya Tank <priyatank2301@gmail.com>
2024-10-15 23:27:32 +0530

    Login Logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,12 @@
-from .views import RegistrationView,UsernameValidationView,EmailValidationView,VerificationView,LoginView
+from .views import RegistrationView,UsernameValidationView,EmailValidationView,VerificationView,LoginView,LogoutView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
 
 urlpatterns = [
-    path('register/', LoginView.as_view(), name='register'),
-     path('login/', RegistrationView.as_view(), name='login'),
-   
+    path('register/', RegistrationView.as_view(), name="register"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    
     path('validate-username/', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email/',csrf_exempt (EmailValidationView.as_view()), name='validate-email'),
     path('activate/<uidb64>/<token>',csrf_exempt (VerificationView.as_view()), name='activate'),
--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
 
-#for view expense
+@login_required(login_url='/authentication/login')
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def index(request):
     return render(request, 'expenses/index.html')
+
 
 # for add expense
 def add_expense(request):
