05b97a2627185133acb7f1fb109428cd41b8f1a9
Priya Tank <priyatank2301@gmail.com>
2024-10-18 00:48:36 +0530

    User Preferences

--- incomeexpensewebsite/settings.py
+++ incomeexpensewebsite/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses',
+    'userpreferences',
 ]
 
 MIDDLEWARE = [
