fc0e64e7edaffde1b5b8b5dd4a7019fa7d015f92
Priya Tank <priyatank2301@gmail.com>
2024-11-25 22:16:48 +0530

    admin customize

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Expense,Category
+from .models import Expense, Category
 
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'owner', 'amount', 'category', 'description')
+    search_fields = ('date', 'owner__username', 'amount', 'description')  # Adjusted category field
+    list_per_page=10
 # Register your models here.
-admin.site.register(Expense)
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
